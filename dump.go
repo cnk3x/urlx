cdaeac687a8c647409ac7af03a920d4e2b60e689
wen <wena@111.com>
2022-01-24 13:30:18 +0800

    fi: dump

--- dump.go
+++ dump.go
@@ -7,35 +7,32 @@ import (
 	"net/http/httputil"
 )
 
-var dumpBR = []byte{'\n'}
-var dumpLine = append(bytes.Repeat([]byte("-"), 70), dumpBR...)
+var (
+	dumpBR   = []byte{'\n'}
+	dumpLine = append(bytes.Repeat([]byte("-"), 70), dumpBR...)
+)
 
 func Dump(w io.Writer, reqBody, respBody bool) ProcessMw {
 	return func(next Process) Process {
 		return func(resp *http.Response) error {
-			re := next(resp)
-
-			if resp != nil {
-				w.Write(dumpLine)
-				reqDump, err := httputil.DumpRequest(resp.Request, reqBody)
-				if err != nil {
-					w.Write([]byte(err.Error()))
-				} else {
-					w.Write(reqDump)
-				}
-				w.Write(dumpBR)
-				w.Write(dumpLine)
-				resDump, _ := httputil.DumpResponse(resp, respBody)
-				if err != nil {
-					w.Write([]byte(err.Error()))
-				} else {
-					w.Write(resDump)
-				}
-				w.Write(dumpBR)
-				w.Write(dumpLine)
+			w.Write(dumpLine)
+			reqDump, err := httputil.DumpRequest(resp.Request, reqBody)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+			} else {
+				w.Write(reqDump)
 			}
-
-			return re
+			w.Write(dumpBR)
+			w.Write(dumpLine)
+			resDump, _ := httputil.DumpResponse(resp, respBody)
+			if err != nil {
+				w.Write([]byte(err.Error()))
+			} else {
+				w.Write(resDump)
+			}
+			w.Write(dumpBR)
+			w.Write(dumpLine)
+			return next(resp)
 		}
 	}
 }
--- version.go
+++ version.go
@@ -10,16 +10,13 @@ import (
 
 const Version = "0.0.1"
 
-var DefaultUserAgent HeaderOption
-
-func init() {
+func DefaultUserAgent() HeaderOption {
 	type x struct{}
-	dua := fmt.Sprintf("urlx/%s (%s) golang/%s(%s %s)", Version, reflect.TypeOf(x{}).PkgPath(), runtime.Version(), runtime.GOOS, runtime.GOARCH)
-	DefaultUserAgent = UserAgent(dua)
+	return UserAgent(fmt.Sprintf("urlx/%s (%s) golang/%s(%s %s)", Version, reflect.TypeOf(x{}).PkgPath(), runtime.Version(), runtime.GOOS, runtime.GOARCH))
 }
 
 // Default 默认的请求器
 func Default(ctx context.Context) *Request {
 	ms := time.Millisecond
-	return New(ctx).HeaderWith(DefaultUserAgent).TryAt(ms*300, ms*800, ms*1500)
+	return New(ctx).HeaderWith(DefaultUserAgent()).TryAt(ms*300, ms*800, ms*1500)
 }
